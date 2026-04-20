41e351e92bc7b39943a004ad6d638ab88c67973e
hoanghieu24 <162785962+hoanghieu24@users.noreply.github.com>
2026-04-20 16:50:06 +0700

    Update categoryRoutes.js

--- bedl/routes/categoryRoutes/categoryRoutes.js
+++ bedl/routes/categoryRoutes/categoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CategoryController = require("../../controllers/category/CategoryController");
+const CategoryController = require("../../controllers/category/categoryController");
 
 // ========== CATEGORY TYPE ROUTES ==========
 
