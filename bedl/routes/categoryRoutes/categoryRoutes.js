d0689fe7e0f3743073592763396dbda857e6543c
hoanghieu24 <162785962+hoanghieu24@users.noreply.github.com>
2026-04-20 16:26:52 +0700

    Fix case sensitivity in CategoryController import

--- bedl/routes/categoryRoutes/categoryRoutes.js
+++ bedl/routes/categoryRoutes/categoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CategoryController = require("../../controllers/Category/CategoryController");
+const CategoryController = require("../../controllers/category/CategoryController");
 
 // ========== CATEGORY TYPE ROUTES ==========
 
