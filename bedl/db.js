239d90e6e6ef617197eb2e88160e68b2afcd1c57
hoanghieu24 <162785962+hoanghieu24@users.noreply.github.com>
2026-04-21 11:27:08 +0700

    Update db.js

--- bedl/db.js
+++ bedl/db.js
@@ -1,12 +1,24 @@
 const mysql = require("mysql2");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("MySQL connection failed:", err.message);
+  } else {
+    console.log("MySQL connected successfully");
+    connection.release();
+  }
 });
 
 module.exports = pool.promise();
